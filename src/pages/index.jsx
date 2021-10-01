3337fbb62d3edd45820c84847161985b21345073
Franklin <franklinq8@gmail.com>
2021-09-30 21:41:40 -0500

    Borrando proyecto react desactualizado

--- src/pages/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-function Index() {
-    return (
-            <section>
-               <span>Empezamos...</span>
-            </section>
-    );
-}
-export default Index;
