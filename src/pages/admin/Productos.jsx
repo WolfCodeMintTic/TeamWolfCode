77c034fd29fbebcf803ef9e6c883ef6e28eeae2d
JesusTorres02 <jesusdanieltorres2002@gmail.com>
2021-10-08 19:26:29 -0500

    Branchreparada

--- src/pages/admin/Productos.jsx
+++ src/pages/admin/Productos.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import {Table, Button, Container, Modal, ModalBody, ModalFooter, ModalHeader, FormGroup} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'assets/productos.css';
+import 'components/Sidebar.jsx';
 
-const Productos = () => {
-    return (
-        <div>
-           crud productos
-        </div>
-    )
-}
+const data =[
+    { ID: 1, Producto:"Camisa", Descripcion:"Estandar",Valor:"$78.000",Estado:"Disponible" }
+   
+];
 
-export default Productos
+class Admin extends React.Component{
+   state={
+       data:data
+   }
+    
+    render(){
+        return(<> 
+        <Container>
+        <br />    
+        <Button color="primary">Registrar Producto</Button>
+        <br /><br />
+        <Table>
+            <thead><tr><th>Id</th>
+            <th>Prducto</th>
+            <th>Descripcion</th>
+            <th>Valor</th>
+            <th></th></tr></thead>
+            <tbody></tbody>
+        </Table>
+    </Container>
+        </>)
+    }
+ 
+            
+        
+    }
+
+
+export default Admin
